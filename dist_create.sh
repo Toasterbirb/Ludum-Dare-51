5bd44a5e5a18494b6032e987ed3dde9678183f8d
Toasterbirb <git@toasterbirb.com>
2022-10-03 18:09:43 +0300

    Get rid of the hacky lib bundling

--- dist_create.sh
+++ dist_create.sh
@@ -10,15 +10,9 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DSTATIC_SDL=On
 make -j$(nproc)
 
 mkdir diamond-heist
-mkdir diamond-heist/libs
 
 # Move the binary and resources
 cp ./diamond_heist ./diamond-heist/
 cp -r ./res ./diamond-heist/
-
-# Copy in 64-bin libsdl from the host system
-cp /usr/lib64/libSDL2-2.0.so.0.2400.0 ./diamond-heist/libs/libSDL2-2.0.so.0
-echo "#!/bin/sh\nLD_LIBRARY_PATH=./libs/ ./diamond_heist" > ./diamond-heist/run_portable.sh
-chmod +x ./diamond-heist/run_portable.sh
 
 zip -r ./diamond-heist.zip ./diamond-heist/
